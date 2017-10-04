bfba8ede30b62aeebd874bb12753ae52411113ee
Hafiz Junaid Shahid <hafizjunaidshahid@gmail.com>
2017-10-05 00:45:57 +0500

    [Add][BorrowerClass] allow to create only one object

--- BookSingleton.php
+++ BookSingleton.php
@@ -2,9 +2,37 @@
 
 class BookSingleton
 {
+    private $author = 'Jun';
+    private $title  = 'Design Patterns';
+    private static $book = NULL;
+    private static $isLoanedOut = FALSE;
+
     private function __construct()
     {
-        echo 'calling';
+
     }
 
+    static function borrowBook() {
+        if (FALSE == self::$isLoanedOut) {
+            if (NULL == self::$book) {
+                self::$book = new BookSingleton();
+            }
+            self::$isLoanedOut = TRUE;
+            return self::$book;
+        } else {
+            return NULL;
+        }
+    }
+
+    function returnBook(BookSingleton $bookReturned) {
+        self::$isLoanedOut = FALSE;
+    }
+
+    function getAuthor() {return $this->author;}
+
+    function getTitle() {return $this->title;}
+
+    function getAuthorAndTitle() {
+        return $this->getTitle() . ' by ' . $this->getAuthor();
+    }
 }
--- index.php
+++ index.php
@@ -4,7 +4,34 @@ spl_autoload_register(function ($class_name) {
 });
 
 
-writeln('setup singleton design pattern');
+writeln('BEGIN TESTING SINGLETON PATTERN');
+writeln('');
+
+$bookBorrower1 = new BookBorrower();
+$bookBorrower2 = new BookBorrower();
+
+$bookBorrower1->borrowBook();
+writeln('BookBorrower1 asked to borrow the book');
+writeln('BookBorrower1 Author and Title: ');
+writeln($bookBorrower1->getAuthorAndTitle());
+writeln('');
+
+$bookBorrower2->borrowBook();
+writeln('BookBorrower2 asked to borrow the book');
+writeln('BookBorrower2 Author and Title: ');
+writeln($bookBorrower2->getAuthorAndTitle());
+writeln('');
+
+$bookBorrower1->returnBook();
+writeln('BookBorrower1 returned the book');
+writeln('');
+
+$bookBorrower2->borrowBook();
+writeln('BookBorrower2 Author and Title: ');
+writeln($bookBorrower1->getAuthorAndTitle());
+writeln('');
+
+writeln('END TESTING SINGLETON PATTERN');
 
 function writeln($line_in)
 {
